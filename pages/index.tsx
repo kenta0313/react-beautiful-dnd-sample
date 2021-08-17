baf2c061d62fb34b72069fed09cbb39012368120
kenta0313 <asanokenta@me.com>
2021-08-17 14:06:26 +0900

    ✨dnd機能追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,30 @@
 import type { NextPage } from 'next'
+import { CHARACTERS } from './caractersData'
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
 const Home: NextPage = () => {
   return (
     <div>
       <h1>react-beautiful-dnd-sample</h1>
+      <DragDropContext>
+        <Droppable droppableId="characters">
+          {(provided) => (
+            <div {...provided.droppableProps} ref={provided.innerRef}>
+              {CHARACTERS.map(({ id, name }, index) => {
+                return (
+                  <Draggable key={id} draggableId={id} index={index}>
+                    {(provided) => (
+                      <li ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                        <p>{name}</p>
+                      </li>
+                    )}
+                  </Draggable>
+                );
+              })}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
     </div>
   )
 }
